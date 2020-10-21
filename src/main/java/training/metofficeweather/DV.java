63031add585bd660fdb7e57285c152ff73d7103a
Harvey <hj_martin@sky.com>
2020-10-21 16:10:10 +0100

    This is the second commit I am adding code to pull live data info from the API

---
+++ src/main/java/training/metofficeweather/DV.java
@@ -0,0 +1,108 @@
+package training.metofficeweather;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DV {
+    @SerializedName(value="F")
+    private String feelsLike;
+    @SerializedName(value="G")
+    private String windGust;
+    @SerializedName(value="H")
+    private String relativeHumidity;
+    @SerializedName(value="T")
+    private String temperature;
+    @SerializedName(value="V")
+    private String visibility;
+    @SerializedName(value="D")
+    private String windDirection;
+    @SerializedName(value="S")
+    private String windSpeed;
+    @SerializedName(value="U")
+    private String maxUV;
+    @SerializedName(value="W")
+    private String weatherType;
+    @SerializedName(value="Pp")
+    private String precipitationProbability;
+
+    public String getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(String feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public String getWindGust() {
+        return windGust;
+    }
+
+    public void setWindGust(String windGust) {
+        this.windGust = windGust;
+    }
+
+    public String getRelativeHumidity() {
+        return relativeHumidity;
+    }
+
+    public void setRelativeHumidity(String relativeHumidity) {
+        this.relativeHumidity = relativeHumidity;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getMaxUV() {
+        return maxUV;
+    }
+
+    public void setMaxUV(String maxUV) {
+        this.maxUV = maxUV;
+    }
+
+    public String getWeatherType() {
+        return weatherType;
+    }
+
+    public void setWeatherType(String weatherType) {
+        this.weatherType = weatherType;
+    }
+
+    public String getPrecipitationProbability() {
+        return precipitationProbability;
+    }
+
+    public void setPrecipitationProbability(String precipitationProbability) {
+        this.precipitationProbability = precipitationProbability;
+    }
+
+
+}
--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String args[]) throws IOException {
         Client client = ClientBuilder.newClient();
         String json = client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=aa34d441-b333-4a68-abd8-4904d5971cfb")
+        String json1 = client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/14?res=3hourly&key=aa34d441-b333-4a68-abd8-4904d5971cfb")
                 .request(MediaType.TEXT_PLAIN)
                 .get(String.class);
 //        Gson gson = new Gson();
@@ -28,6 +29,19 @@ public class Main {
         Root root = gson.fromJson(json, Root.class);
         Locations locations = root.getLocations();
         Location[] locationsLocations = locations.getLocation();
-        System.out.println(locationsLocations[0].getName());
+        System.out.println("Location Name: " + locationsLocations[0].getName());
+        System.out.println("Region: " + locationsLocations[0].getRegion());
+        System.out.println("Auth Area: " + locationsLocations[0].getUnitaryAuthArea());
+        System.out.println("Longitude: " + locationsLocations[0].getLongitude());
+        System.out.println("Latitude: " + locationsLocations[0].getLatitude());
+        System.out.println("Elevation: " + locationsLocations[0].getElevation());
+        System.out.println("ID: " + locationsLocations[0].getId());
+
+        Gson gson1 =new Gson();
+
+        Root DV = gson1.fromJson(json1, Root.class);
+
+
+
     }
 }	
